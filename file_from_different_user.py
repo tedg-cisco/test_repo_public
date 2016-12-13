e97a3fc83ddb6cc8fd4aab41261b73248619fc11
COSO 3C <cosothreec@users.noreply.github.com>
2016-12-13 09:54:59 -0800

    Create file_from_different_user.py
    This is an example of a user creating a new file in another user's repo

---
+++ file_from_different_user.py
@@ -0,0 +1,13 @@
+print("Hello world")
+	
+a = True
+if a == True:
+	print("It is true!")
+else:
+	print("It is false...")
+
+b = raw_input("Enter value:")
+print("Your value is")
+print(b)
+
+raw_input("Press enter to continue...")
